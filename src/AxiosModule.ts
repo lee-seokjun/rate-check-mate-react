aad617f85fbe658a162ebeca1636a87d9943a6fd
seokjun <iope100@naver.com>
2023-11-19 23:24:11 +0900

    운영 설정으로 커밋

--- src/AxiosModule.ts
+++ src/AxiosModule.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL:"http://127.0.0.1:8080",
+  baseURL:"http://44.204.49.80:8080",
+  // baseURL:"http://127.0.0.1:8080",
 });
 
 export default instance;
